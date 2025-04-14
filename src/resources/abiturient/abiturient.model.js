4200ce177759b7eddd1986d743f62d80c67a04c2
Sasha Borisov <35650271+AlexPos1tivuss@users.noreply.github.com>
2025-04-14 05:53:53 +0000

    add models

--- src/resources/abiturient/abiturient.model.js
+++ src/resources/abiturient/abiturient.model.js
@@ -1,17 +1,10 @@
-import { v4 as uuidv4 } from 'uuid';
-
-class User {
-  constructor({ id = uuidv4(), name = 'USER', login = 'user', password = 'P@55w0rd' } = {}) {
+class Abiturient {
+  constructor(id, name, surname, birthDate) {
     this.id = id;
     this.name = name;
-    this.login = login;
-    this.password = password;
-  }
-
-  static toResponse(user) {
-    const { id, name, login } = user;
-    return { id, name, login };
+    this.surname = surname;
+    this.birthDate = birthDate;
   }
 }
 
-export default User;
+module.exports = Abiturient;
